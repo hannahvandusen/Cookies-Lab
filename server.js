31cfe803620fd3726bc13d081f6eee1413148ca1
Hannah Van Dusen <hannahvandusen@yahoo.com>
2023-01-28 15:26:33 -0500

    cookies update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express'); 
 const app = express();
 const PORT = 4010; 
+const index = require('./public/index'); 
 const cookieParser = require('cookie-parser'); 
 
 app.use(cookieParser()); 
 
-
+app.use('/cookie-jar', index); 
 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true })); 
